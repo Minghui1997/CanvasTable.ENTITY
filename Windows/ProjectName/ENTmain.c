158e286671aacf7cb40a02c76f8f9dc10669f1c4
MingHui <103197921+MingHui1997@users.noreply.github.com>
2023-04-20 18:05:54 +0800

    Add files via upload

---
+++ Windows/ProjectName/ENTmain.c
@@ -0,0 +1,21 @@
+﻿#include "CanvasTable.h"
+
+void CT_main();
+void CT_loop();
+
+void ENTITYmain()
+{
+    char *ResList[] = {""};
+
+    CT_init(960,540,ResList,sizeof(ResList),CT_main,CT_loop,1,0,1,960,540,0,0,0,0,0,0,0,0,0,0,0);
+}
+
+void CT_main()
+{
+
+}
+
+void CT_loop()
+{
+
+}
